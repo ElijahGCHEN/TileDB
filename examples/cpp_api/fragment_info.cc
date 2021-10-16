a48764748a1ac0bd257ac8ac4a0225f97a8630a2
ithiria894 <63032496+ithiria894@users.noreply.github.com>
2021-10-16 17:29:58 +0800

    Update fragment_info.cc

--- examples/cpp_api/fragment_info.cc
+++ examples/cpp_api/fragment_info.cc
@@ -84,7 +84,7 @@ void write_array() {
   // }
 
   //
-  std::vector<int> subarray = {1, 3, 1, 4};
+  std::vector<int> subarray = {1, 2, 1, 4};
 
   //std::vector<int> subarray = {1, 1, 100, 160};
   //std::vector<int> subarray = {1, 1, 1, 61};
@@ -94,7 +94,7 @@ void write_array() {
   Query query(ctx, array);
   query.set_layout(TILEDB_ROW_MAJOR)
       .set_buffer("a", data)
-      //.set_subarray(subarray);
+      .set_subarray(subarray);
 
   // Perform the write and close the array.
   //query.submit();
@@ -109,7 +109,7 @@ void write_array() {
   //Query query(ctx, array);
   query.set_layout(TILEDB_ROW_MAJOR)
       .set_buffer("a", data1)
-      //.set_subarray(subarray1);
+      .set_subarray(subarray1);
 
   // Perform the write and close the array.
   query.submit();
