be32548da08f339ef130cc90032717606809fee7
ithiria894 <63032496+ithiria894@users.noreply.github.com>
2021-10-30 19:36:11 +0800

    v

--- examples/cpp_api/graph/graph.h
+++ examples/cpp_api/graph/graph.h
@@ -81,7 +81,7 @@ class Graph{
   /* ********************************* */
 
 
-	void insert(std::initializer_list<Vertex> bases, Vertex* n);
+	void insert(std::vector<Vertex> bases, Vertex* n);
 	void add_child(Vertex* newParent, Vertex* n);
 	void materialization(int i);
   void set_materialized(int i);
