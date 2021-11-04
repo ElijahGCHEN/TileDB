1b5a197d3a46b1d156b1ce932021105663e65a2b
ithiria894 <63032496+ithiria894@users.noreply.github.com>
2021-11-04 18:22:34 +0800

    Update fragment_info.cc

--- examples/cpp_api/fragment_info.cc
+++ examples/cpp_api/fragment_info.cc
@@ -689,7 +689,8 @@ void createFragment(int x1,int y1,int x2,int y2,std::vector<std::vector<std::pai
   //Vertex VerNew(*non_empty_vector.end(),*uri.end(),graph.vertexs.size(),*num_of_cells.end(),*timestamps_vector.end());
   Vertex VerNew(non_empty_vector[index],uri[index],graph.vertexs.size(),num_of_cells[index],timestamps_vector[index]);
   std::cout<<"------line691----"<<std::endl;
-  auto parentvertex=*graph.vertexs.end();
+  
+  auto parentvertex=graph.vertexs[graph.vertexs.size()-1];
   std::cout<<"------line693----"<<std::endl;
   std::vector<Vertex*> vparent(1,&parentvertex);
   std::cout<<"------line695----"<<std::endl;
