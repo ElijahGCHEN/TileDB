efcba0bf71a318e427337255c2eeecb3f4d0b034
ithiria894 <63032496+ithiria894@users.noreply.github.com>
2021-11-05 19:54:46 +0800

    Update graph.cc

--- examples/cpp_api/graph/graph.cc
+++ examples/cpp_api/graph/graph.cc
@@ -532,7 +532,7 @@ void Graph::DFS(int v,int sum_of_weight,int thresh,int &Maxheight, int currentH)
                 // DFS(*i);   
                 //visited[v][i] = true;
 
-                DFS(i,sum_of_weight,thresh,Maxheight, currentH+1);             
+                visit(i,Maxheight, currentH+1);             
             //}
         }
         //DFS(i);  
